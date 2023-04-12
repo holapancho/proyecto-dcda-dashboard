cf541d584cb8ac1be7a97fab8e667d28dee95a36
Francisco Rojas <francisco.rojas.morales@gmail.com>
2023-04-11 21:05:58 -0400

    CSVs leibles

--- fuentes.py
+++ fuentes.py
@@ -6,6 +6,7 @@ class DatosConcurso:
     self.country_data = pd.read_csv("./csv_fuentes/country_data.csv")
     self.song_data = pd.read_csv("./csv_fuentes/song_data.csv")
     self.datos_resultados_finales = DatosResultadosFinales()
+    self.datos_encuestas = DatosEncuestas()
 
 class DatosResultadosFinales:
   def __init__(self):
@@ -29,3 +30,54 @@ class DatosTelevotacion:
     self.televote_results_2019 = pd.read_csv("./csv_fuentes/resultados_finales/televotacion/2019_televote_results.csv")
     self.televote_results_2021 = pd.read_csv("./csv_fuentes/resultados_finales/televotacion/2021_televote_results.csv")
     self.televote_results_2022 = pd.read_csv("./csv_fuentes/resultados_finales/televotacion/2022_televote_results.csv")
+
+class DatosEncuestas:
+  def __init__(self) -> None:
+    self.euro_july = EuroJuly()
+    self.eurovision_world = EurovisionWorld()
+    self.my_eurovision_score = MyEurovisionScore()
+    self.ogae = OGAE()
+    self.wiwibloggs = Wiwibloggs()
+    
+class EuroJuly:
+  def __init__(self):
+    self.eurojury_results_2016 = pd.read_csv("./csv_fuentes/encuestas/euro_july/2016_eurojury_results.csv")
+    self.eurojury_results_2017 = pd.read_csv("./csv_fuentes/encuestas/euro_july/2017_eurojury_results.csv")
+    self.eurojury_results_2018 = pd.read_csv("./csv_fuentes/encuestas/euro_july/2018_eurojury_results.csv")
+    self.eurojury_results_2019 = pd.read_csv("./csv_fuentes/encuestas/euro_july/2019_eurojury_results.csv")
+    self.eurojury_results_2021 = pd.read_csv("./csv_fuentes/encuestas/euro_july/2021_eurojury_results.csv")
+    self.eurojury_results_2022 = pd.read_csv("./csv_fuentes/encuestas/euro_july/2022_eurojury_results.csv")
+
+class EurovisionWorld:
+  def __init__(self):
+    self.eurovisionworld_results_2016 = pd.read_csv("./csv_fuentes/encuestas/eurovision_world/2016_eurovisionworld_results.csv")
+    self.eurovisionworld_results_2017 = pd.read_csv("./csv_fuentes/encuestas/eurovision_world/2017_eurovisionworld_results.csv")
+    self.eurovisionworld_results_2018 = pd.read_csv("./csv_fuentes/encuestas/eurovision_world/2018_eurovisionworld_results.csv")
+    self.eurovisionworld_results_2019 = pd.read_csv("./csv_fuentes/encuestas/eurovision_world/2019_eurovisionworld_results.csv")
+    self.eurovisionworld_results_2021 = pd.read_csv("./csv_fuentes/encuestas/eurovision_world/2021_eurovisionworld_results.csv")
+    self.eurovisionworld_results_2022 = pd.read_csv("./csv_fuentes/encuestas/eurovision_world/2022_eurovisionworld_results.csv")
+
+class MyEurovisionScore:
+  def __init__(self):
+    self.myeurovisionscoreboard_results_2016 = pd.read_csv("./csv_fuentes/encuestas/my_eurovision_score/2016_myeurovisionscoreboard_results.csv")
+    self.myeurovisionscoreboard_results_2017 = pd.read_csv("./csv_fuentes/encuestas/my_eurovision_score/2017_myeurovisionscoreboard_results.csv")
+    self.myeurovisionscoreboard_results_2018 = pd.read_csv("./csv_fuentes/encuestas/my_eurovision_score/2018_myeurovisionscoreboard_results.csv")
+    self.myeurovisionscoreboard_results_2019 = pd.read_csv("./csv_fuentes/encuestas/my_eurovision_score/2019_myeurovisionscoreboard_results.csv")
+    self.myeurovisionscoreboard_results_2021 = pd.read_csv("./csv_fuentes/encuestas/my_eurovision_score/2021_myeurovisionscoreboard_results.csv")
+    self.myeurovisionscoreboard_results_2022 = pd.read_csv("./csv_fuentes/encuestas/my_eurovision_score/2022_myeurovisionscoreboard_results.csv")
+
+class OGAE:
+  def __init__(self):
+    self.ogae_results_2016 = pd.read_csv("./csv_fuentes/encuestas/ogae/2016_ogae_results.csv")
+    self.ogae_results_2017 = pd.read_csv("./csv_fuentes/encuestas/ogae/2017_ogae_results.csv")
+    self.ogae_results_2018 = pd.read_csv("./csv_fuentes/encuestas/ogae/2018_ogae_results.csv")
+    self.ogae_results_2019 = pd.read_csv("./csv_fuentes/encuestas/ogae/2019_ogae_results.csv")
+    self.ogae_results_2021 = pd.read_csv("./csv_fuentes/encuestas/ogae/2021_ogae_results.csv")
+    self.ogae_results_2022 = pd.read_csv("./csv_fuentes/encuestas/ogae/2022_ogae_results.csv")
+
+class Wiwibloggs:
+  def __init__(self):
+    self.wiwibloggs_results_2018 = pd.read_csv("./csv_fuentes/encuestas/wiwibloggs/2018_wiwibloggs_results.csv")
+    self.wiwibloggs_results_2019 = pd.read_csv("./csv_fuentes/encuestas/wiwibloggs/2019_wiwibloggs_results.csv")
+    self.wiwibloggs_results_2021 = pd.read_csv("./csv_fuentes/encuestas/wiwibloggs/2021_wiwibloggs_results.csv")
+    self.wiwibloggs_results_2022 = pd.read_csv("./csv_fuentes/encuestas/wiwibloggs/2022_wiwibloggs_results.csv")
