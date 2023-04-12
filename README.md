eddf49d3bf2abf79e346955337c0acb39417f7d4
Francisco Rojas <francisco.rojas.morales@gmail.com>
2023-04-11 20:19:02 -0400

    leyendo resultados finales en CSV

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # proyecto-dcda-dashboard
 Proyecto para finalizar el curso de Visualizacion de Datos. El proyecto corre de manera local, las librerias se manejaron con Conda. Ademas esta instalado Jupyter Notebook para realizar pruebas locales.
 
+Dataset utilizado 
+https://www.kaggle.com/datasets/diamondsnake/eurovision-song-contest-data?datasetId=2942792
+
+Se sanitizó el archivo <code>song_data.csv</code> para trabajar con Pandas en UTF-8.
+
 ## Precondiciones
 
 *  Conda instalado
--- fuentes.py
+++ fuentes.py
@@ -2,4 +2,30 @@ import pandas as pd
 
 class DatosConcurso:
   def __init__(self):
-    self.contest_data = pd.read_csv("./csv_fuentes/contest_data.csv") 
+    self.contest_data = pd.read_csv("./csv_fuentes/contest_data.csv")
+    self.country_data = pd.read_csv("./csv_fuentes/country_data.csv")
+    self.song_data = pd.read_csv("./csv_fuentes/song_data.csv")
+    self.datos_resultados_finales = DatosResultadosFinales()
+
+class DatosResultadosFinales:
+  def __init__(self):
+    self.datos_jurado = DatosJurado()
+    self.datos_televotacion = DatosTelevotacion()
+
+class DatosJurado:
+  def __init__(self):
+    self.jury_results_2016 = pd.read_csv("./csv_fuentes/resultados_finales/jurado/2016_jury_results.csv")
+    self.jury_results_2017 = pd.read_csv("./csv_fuentes/resultados_finales/jurado/2017_jury_results.csv")
+    self.jury_results_2018 = pd.read_csv("./csv_fuentes/resultados_finales/jurado/2018_jury_results.csv")
+    self.jury_results_2019 = pd.read_csv("./csv_fuentes/resultados_finales/jurado/2019_jury_results.csv")
+    self.jury_results_2021 = pd.read_csv("./csv_fuentes/resultados_finales/jurado/2021_jury_results.csv")
+    self.jury_results_2022 = pd.read_csv("./csv_fuentes/resultados_finales/jurado/2022_jury_results.csv")
+
+class DatosTelevotacion:
+  def __init__(self):
+    self.televote_results_2016 = pd.read_csv("./csv_fuentes/resultados_finales/televotacion/2016_televote_results.csv")
+    self.televote_results_2017 = pd.read_csv("./csv_fuentes/resultados_finales/televotacion/2017_televote_results.csv")
+    self.televote_results_2018 = pd.read_csv("./csv_fuentes/resultados_finales/televotacion/2018_televote_results.csv")
+    self.televote_results_2019 = pd.read_csv("./csv_fuentes/resultados_finales/televotacion/2019_televote_results.csv")
+    self.televote_results_2021 = pd.read_csv("./csv_fuentes/resultados_finales/televotacion/2021_televote_results.csv")
+    self.televote_results_2022 = pd.read_csv("./csv_fuentes/resultados_finales/televotacion/2022_televote_results.csv")
