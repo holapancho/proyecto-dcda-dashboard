1b325e33e5c95fe269bffbf0a1168f873efe1a0f
Francisco Rojas <francisco.rojas.morales@gmail.com>
2023-04-16 12:03:38 -0400

    agrega dependencia dash bootstrap

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from dash import Dash, html, dcc, callback, Output, Input
+import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
 from fuentes import DatosConcurso
