4542fe69e576d4dea4c2e6f1f28657531d888066
Francisco Rojas <francisco.rojas.morales@gmail.com>
2023-04-23 12:37:15 -0400

    modificar barchart por pie

--- app.py
+++ app.py
@@ -20,33 +20,55 @@ app.layout = dbc.Container(
                 dbc.Col(html.Img(src='assets/dataset-cover.png',
                         style={'height': '100px'}), md=1),
                 dbc.Col(md=9),
-                dbc.Col(dcc.Dropdown(anios_string,
-                                     anio_por_defecto,
-                                     searchable=False,
+                dbc.Col(dcc.Dropdown(options=anios_string,
+                                     value=anio_por_defecto,
+                                     clearable=False,
                                      id='demo-dropdown'),
                         md=2)
             ]),
-        # Primer Registro - cabecera
+        # Segundo Registro 
         dbc.Row(
             [
-                dbc.Col(dcc.Graph(id='grafico-1'), md=6),
-                dbc.Col(md=6)
+                dbc.Col(dcc.Graph(id='grafico-1'), md=12)
+            ]
+        ),
+         # Tercer Registro 
+        dbc.Row(
+            [
+                dbc.Col(dcc.Graph(id='grafico-2'), md=12)
             ]
         )
     ],
     fluid=True
 )
 
+#documentacion https://dash.plotly.com/interactive-graphing
+#The dcc.Graph component has four attributes that can change through user-interaction: hoverData, clickData, selectedData, relayoutData. These properties update when you hover over points, click on points, or select regions of points in a graph.
+
 
 @app.callback(
     Output('grafico-1', 'figure'),
     Input('demo-dropdown', 'value')
 )
-def actualiza_grafico_por_anio(value):
+def actualiza_pie_plot_por_anio(value):
     if value == 'None':
         value = anio_por_defecto
     return plots.obtener_pie_plot_por_anio(value)
 
+@app.callback(
+    Output('grafico-2', 'figure'),
+    Input('demo-dropdown', 'value'),
+    Input(component_id='grafico-1', component_property='relayoutData') 
+)
+def actualiza_bar_chart_resultados_finales_por_anio(value,relayoutData):
+    hiddenlabels = []
+    if value == 'None':
+        value = anio_por_defecto
+
+    if isinstance(relayoutData, dict):
+        if 'hiddenlabels' in relayoutData:
+            hiddenlabels = relayoutData['hiddenlabels']
+    return plots.obtener_bar_chart_resultados_finales_por_anio(value,hiddenlabels)
 
 if __name__ == '__main__':
     app.run_server(debug=True)
--- diagramas.py
+++ diagramas.py
@@ -12,6 +12,25 @@ class Plots:
         song_data = self.datos_concurso.song_data
         song_data_2022 = song_data[(song_data.year == anio) & (song_data.final_place > 0.0)]
         song_data_2022_top = song_data_2022.sort_values(by='final_place',ascending=True)
-        fig = px.pie(song_data_2022_top.head(), values='final_total_points', names='country', title='Paises de los artistas con mas votos el año {}'.format(anio))
+        fig = px.pie(song_data_2022_top, values='final_total_points', names='country', title='Paises de los artistas con mas votos el año {}'.format(anio))
+        fig.update_layout(transition_duration=500)
+        return fig
+    
+    def obtener_bar_chart_resultados_finales_por_anio(self, anio_string, excluded_countries):
+        df = None
+        if anio_string == '2022':
+            df = self.datos_concurso.datos_resultados_finales.datos_televotacion.televote_results_2022
+        elif anio_string == '2021':
+            df = self.datos_concurso.datos_resultados_finales.datos_televotacion.televote_results_2021
+        elif anio_string == '2019':
+            df = self.datos_concurso.datos_resultados_finales.datos_televotacion.televote_results_2019
+        elif anio_string == '2018':
+            df = self.datos_concurso.datos_resultados_finales.datos_televotacion.televote_results_2018
+        elif anio_string == '2017':
+            df = self.datos_concurso.datos_resultados_finales.datos_televotacion.televote_results_2017
+        elif anio_string == '2016':
+            df = self.datos_concurso.datos_resultados_finales.datos_televotacion.televote_results_2016
+
+        fig = px.bar(df.loc[~df['Contestant'].isin(excluded_countries)], x="Contestant", y=["Jury score", "Televoting score"], title="Puntos por concursante en el año {}".format(anio_string))
         fig.update_layout(transition_duration=500)
         return fig
